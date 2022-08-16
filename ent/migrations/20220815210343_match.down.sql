fe749c45473c854f306d268808b539a686ebfd20
fabian <fbngrm@gmail.com>
2022-08-16 11:57:57 +0200

    chore: delete old migrations

--- ent/migrations/20220815210343_match.down.sql
+++
@@ -1,6 +0,0 @@
--- reverse: create "partner_materials" table
-DROP TABLE "partner_materials";
--- reverse: create "materials" table
-DROP TABLE "materials";
--- reverse: create "partners" table
-DROP TABLE "partners";
